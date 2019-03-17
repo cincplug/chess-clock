11f0b3a92f677769b1113ba53930400e79ef67aa
Luka Stanisavljevic <luka@cincplug.com>
2019-03-17 23:54:44 +0100

    Timer layout

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { connect } from "react-redux";
 
-function Timer() {
+const mapStateToProps = state => {
+  return {
+    remainingTime: state.remainingTime
+  };
+};
+
+const ClockBox = styled.div`
+  display: flex;
+  width: 14rem;
+`;
+
+const Clock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0.5rem 1rem;
+  width: 50%;
+  background: ${props => props.bg};
+  color: gray;
+`;
+
+function Timer({ remainingTime }) {
   return (
-    <div>
-      Timer
-    </div>
-  )
+    <ClockBox>
+      <Clock bg="white">
+        <div>White</div>
+        <div>{remainingTime.white}</div>
+      </Clock>
+      <Clock bg="black">
+        <div>Black</div>
+        <div>{remainingTime.black}</div>
+      </Clock>
+    </ClockBox>
+  );
 }
 
-export default Timer
+export default connect(mapStateToProps)(Timer);
