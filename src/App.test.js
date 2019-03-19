b9a0f7fe82d1676f7d6cd772d0ac7e1fe842eeb1
Luka Stanisavljevic <luka@cincplug.com>
2019-03-19 15:43:40 +0100

    External initial state

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { initialState } from '../../config.js';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Provider store={} ><App /></Provider>, div);
+  ReactDOM.render(<Provider store={initialState} ><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,16 +1,4 @@
-const msPerMinute = 60 * 1000;
-const defaultInitialTime = 15 * msPerMinute;
-const initialState = {
-  gameStarted: false,
-  gameEnded: false,
-  remainingTime: {
-    white: defaultInitialTime,
-    black: defaultInitialTime
-  },
-  currentPlayer: "",
-  currentMoveDuration: 0,
-  timeline: []
-};
+import { initialState } from '../../config.js';
 
 function rootReducer(state = initialState, action) {
   switch (action.type) {
